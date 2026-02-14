6c106d4c68ac4342d778513d6ff1623763e12e25
katarem <crismigesp@gmail.com>
2026-02-14 19:11:42 +0000

    exception handling for customer endpoints

---
+++ ms-customers/src/main/java/com/bytecodes/ms_customers/handler/CustomerExceptionHandler.java
@@ -0,0 +1,64 @@
+package com.bytecodes.ms_customers.handler;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class CustomerExceptionHandler {
+
+    @ExceptionHandler(value = {DataIntegrityViolationException.class})
+    public ResponseEntity<Map<String, String>> duplicatedData(DataIntegrityViolationException ex) {
+        
+        String code = "CUSTOMER_CONFLICT";
+        String message = "Ya existe un cliente con características similares.";
+
+        if (ex.getMostSpecificCause() != null &&
+            ex.getMostSpecificCause().getMessage().contains("(dni)")) {
+            message = "El DNI ya está registrado";
+            code = "DUPLICATED_DNI";
+        } else if (ex.getMostSpecificCause().getMessage().contains("(email)")) {
+            message = "El email ya está registrado";
+            code = "DUPLICATED_EMAIL";
+        }
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(Map.of(
+                        "code", code,
+                        "message", message,
+                        "timestamp", Instant.now().toString()
+                ));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> validationError(MethodArgumentNotValidException ex) {
+        StringBuilder sb = new StringBuilder();
+        for (ObjectError error : ex.getAllErrors()) {
+            sb.append(error.getDefaultMessage() + ",");
+        }
+        String errors = sb.toString();
+        String timestamp = Instant.now().toString();
+        String code = "INVALID_FIELDS";
+
+        return ResponseEntity
+            .badRequest()
+            .body(Map.of(
+                "code", code, 
+                "errors", errors, 
+                "timestamp", timestamp
+            ));
+
+    }
+
+}
