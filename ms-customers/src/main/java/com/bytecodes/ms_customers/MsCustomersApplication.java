0083a43ee7159bb63d8c591b5599abb1287ba8a2
katarem <crismigesp@gmail.com>
2026-02-14 12:40:04 +0000

    ms-customers initial commit

---
+++ ms-customers/src/main/java/com/bytecodes/ms_customers/MsCustomersApplication.java
@@ -0,0 +1,13 @@
+package com.bytecodes.ms_customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCustomersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCustomersApplication.class, args);
+	}
+
+}
