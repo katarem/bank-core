c2cf706a9561c456f06ab8ad2b68fd4ff6ea6f11
katarem <crismigesp@gmail.com>
2026-02-16 13:52:36 +0000

    added clock for easier testing

--- ms-customers/src/main/java/com/bytecodes/ms_customers/util/JwtUtil.java
+++ ms-customers/src/main/java/com/bytecodes/ms_customers/util/JwtUtil.java
@@ -1,22 +1,19 @@
 package com.bytecodes.ms_customers.util;
 
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.JwtParser;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.security.saml2.Saml2RelyingPartyProperties;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
+@RequiredArgsConstructor
 public class JwtUtil {
 
     @Value("${jwt.secret}")
@@ -25,6 +22,8 @@ public class JwtUtil {
     @Getter
     @Value("${jwt.expiration}")
     private long expiration;
+
+    private final Clock clock;
 
 
     public String generateToken(Authentication auth) {
@@ -76,7 +75,7 @@ public class JwtUtil {
                 .parseSignedClaims(token);
 
         Date expiredBy = jwt.getPayload().getExpiration();
-        Date now = new Date();
+        Date now = clock.now();
 
         return now.after(expiredBy);
     }
